abc8302d16466b82b696e8b7c6ceb4460fdb7a18
chohj06 <51467453+chohj06@users.noreply.github.com>
2020-06-08 23:58:28 +0900

    Add files via upload

---
+++ hm.py
@@ -0,0 +1,34 @@
+import time
+
+import discord
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(client.user.id)
+    print("ready")
+    game = discord.Game("일")
+    await client.change_presence(status=discord.Status.online, activity=game)
+
+
+@client.event
+async def on_message(message):
+    if message.content.startswith("!구독"):
+        await message.channel.send("https://www.youtube.com/channel/UCLCn7P6JHwacN7owSR_uS1g?sub_confirmation=1")
+
+
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'안녕 {member.name}님, 구독 해주세요! https://www.youtube.com/channel/UCLCn7P6JHwacN7owSR_uS1g?sub_confirmation=1'
+    )
+
+
+
+
+
+
+
+client.run("NzE4ODQ4NzA1NzcxMzM5Nzg3.Xtu7bg.K-tbj2TC55wEqeLc0ewFq3Qjpjs")
